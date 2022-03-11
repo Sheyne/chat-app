0b58586189bdb5514f0a3b4420cdc840e469442b
Sheyne Anderson <sheyne@sheyne.com>
2022-03-11 00:48:29 +0000

    Refactor to use new-style react state

--- typescript/src/RoomList.tsx
+++ typescript/src/RoomList.tsx
@@ -1,56 +1,46 @@
 import './RoomList.css';
-import * as React from 'react';
+import React, { useState } from 'react';
 
-type Props = {
+export function RoomList(props: {
   onSelectionChanged: (name: string) => void;
   onAddRoom: (name: string) => void;
   rooms: string[];
-};
+}) {
 
-export class RoomList extends React.Component<Props, { selected: string, newRoomName: string }> {
+  const [selected, setSelected_] = useState(props.rooms[0]!);
+  const [newRoomName, setNewRoomName] = useState("");
 
-  constructor(props: Props) {
-    super(props)
-
-    this.state = { selected: this.props.rooms[0]!, newRoomName: "" };
+  const setSelected = (name: string) => {
+    setSelected_(name);
+    props.onSelectionChanged(name);
   }
 
-  setSelected(name: string) {
-    this.setState({ selected: name });
-    this.props.onSelectionChanged(name);
-  }
-
-  addRoom(e: React.FormEvent) {
+  const addRoom = (e: React.FormEvent) => {
     e.preventDefault();
-    if (this.state.newRoomName) {
-      this.props.onAddRoom(this.state.newRoomName);
+    if (newRoomName) {
+      props.onAddRoom(newRoomName);
+      setSelected(newRoomName);
     }
-    this.setState({ newRoomName: "" });
+    setNewRoomName("");
   }
 
-  setNewRoomName(e: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({ newRoomName: e.target.value });
-  }
+  const rooms = props.rooms.map(name => <div
+    key={name}
+    onClick={() => setSelected(name)}
+    className={`RoomList-room ${name === selected ? "selected" : ""
+      }`}>{name}</div>)
 
-  override render() {
-    const rooms = this.props.rooms.map(name => <div
-      key={name}
-      onClick={() => this.setSelected(name)}
-      className={`RoomList-room ${name === this.state.selected ? "selected" : ""
-        }`}>{name}</div>)
-
-    return (
-      <div className="RoomList">
-        {rooms}
-        <div className='RoomList-addRoom'>
-          <form onSubmit={e => this.addRoom(e)}>
-            <input type="text" onChange={e => this.setNewRoomName(e)} value={this.state.newRoomName} />
-            <input type="submit" value="+" />
-          </form>
-        </div>
+  return (
+    <div className="RoomList" >
+      {rooms}
+      < div className='RoomList-addRoom' >
+        <form onSubmit={e => addRoom(e)}>
+          <input type="text" onChange={e => setNewRoomName(e.target.value)} value={newRoomName} />
+          <input type="submit" value="+" />
+        </form>
       </div>
-    );
-  }
+    </div >
+  );
 }
 
 export default RoomList;
